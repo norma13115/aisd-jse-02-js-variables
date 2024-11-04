d707de4f9d6c690a8ef6b2358120863729d5e01b
norma13115 <normapadilla13115@gmail.com>
2024-11-03 18:14:36 -0800

    create index js

---
+++ index.js
@@ -0,0 +1,17 @@
+let age = 25; 
+console.log("Initial Age:", age);
+
+const birthYear = 1998;
+console.log("Birth Year:", birthYear);
+
+age = 30;
+console.log("Updated Age:", age);
+
+let currentYear = 2025;
+let yearsSinceBirth = currentYear - birthYear;
+console.log("Years Since Birth:", yearsSinceBirth);
+
+const greeting = "Hello";
+let userName = "Norma";
+let presonalizedGreeting = greeting + " " + userName + "!";
+console.log("Personalized Greeting:", presonalizedGreeting);    
